7651bce4981a4f04950a4a310e0f890f22e35d7f
gracesubianto <gracesubianto@gmail.com>
2024-02-05 18:10:19 +1300

    adding componenets to discover page

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Resources used
 
-- [Build and Deploy a Better Spotify 2.0 Clone Music App with React 18! (Tailwind, Redux, RapidAPI)](https://www.youtube.com/watch?v=I1cpb0tYV74)
+- [Tutorial](https://www.youtube.com/watch?v=I1cpb0tYV74)
 - [node install](https://nodejs.org/en/download/)
 
 ## Rose Bud Thorn Retrospectives
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,19 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongContainer } from "../components";
+import { genres } from "react-icons/h1";
+
+const Discover = () => {
+  const genreTitle = "Pop";
+  console.log(genres);
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select>onChange={() => {}}</select>
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
