ea4cf2db2a0c175855df72b4f5d77ff61dad35c1
gracesubianto <gracesubianto@gmail.com>
2024-02-05 18:17:58 +1300

    adding genre mapping

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -10,8 +10,19 @@ const Discover = () => {
         <h2 className="font-bold text-3xl text-white text-left">
           Discover {genreTitle}
         </h2>
-        <select>onChange={() => {}}</select>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
       </div>
+      <div></div>
     </div>
   );
 };
